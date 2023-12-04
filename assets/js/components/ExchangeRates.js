4f65084b1a6cc3ee591c89fc8e89c08d9c348aa8
Damian Wasiak <damianwasiak49@gmail.com>
2023-12-04 19:32:02 +0100

    add ExchangeRates.js

---
+++ assets/js/components/ExchangeRates.js
@@ -0,0 +1,36 @@
+// ./assets/js/components/Users.js
+
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class ExchangeRates extends Component {
+    constructor() {
+        super();
+        
+    }
+
+    componentDidMount() {
+        
+    }
+
+    checkApiSetup() {
+      
+    }
+
+    render() {
+      
+        return(
+            <div>
+                <section className="row-section">
+                    <div className="container">
+                        <div className="row mt-5"></div>
+                        <div className="col-md-8 offset-md-2 d-flex justify-content-center">
+                            <h1>Exchange Ratest Test</h1>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+export default ExchangeRates;

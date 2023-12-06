d4799602f4fe5760269384062a08be1982e8ff69
Damian Wasiak <damianwasiak49@gmail.com>
2023-12-06 22:51:39 +0100

    respose view

--- assets/js/components/ExchangeRates.js
+++ assets/js/components/ExchangeRates.js
@@ -1,16 +1,15 @@
 import React from 'react';
-// Zaczytuje komponenty funkcyjne odpowiedzialne za renderowanie tabel z aktualnymi kursami oraz z kursami z dnia wybranego przez użytkownika
 import ExchangeRatesToday from './ExchangeRatesToday';
 import ExchangeRatesDate from './ExchangeRatesDate';
+import '../../css/ExchangeRates.css';
 
 const ExchangeRates = () => {
   return (
-    // Kontener dla sekcji z datą i sekcji z dzisiejszymi kursami, używając flexboxa do ustawienia obok siebie
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: 1, marginRight: '20px' }}>
+    <div className="exchange-rates-container">
+      <div className="date-container">
         <ExchangeRatesDate />
       </div>
-      <div style={{ flex: 1 }}>
+      <div className="today-container">
         <ExchangeRatesToday />
       </div>
     </div>
